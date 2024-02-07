d244775f09545527368dcd5cc2e8cd7a56072096
footprintbeaver <tkdlzhmk3@gmail.com>
2024-02-07 09:56:45 +0900

    레벨ui추가

--- Assets/Scripts/Player/CharacterStatus.cs
+++ Assets/Scripts/Player/CharacterStatus.cs
@@ -2,7 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[System.Serializable]
+public class Stats
+{
+    [HideInInspector]
+    public float curValue;
+    public float maxValue;
+    public float startValue;
+    public Image uiBar;
+
+public void Add(float amount)
+{
+    curValue = Mathf.Min(curValue + amount, maxValue);
+}
+
+public void Subtract(float amount)
+{
+    curValue = Mathf.Max(curValue - amount, 0.0f);
+}
+
+public float GetPercentage()
+{
+    return curValue / maxValue;
+}
+
+}
 
 public class CharacterStatus : MonoBehaviour
 {
